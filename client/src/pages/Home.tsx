412d365bf81064fd8d1613a7c2eb2cc9db701b2b
Druue <tileyratkins@gmail.com>
2021-01-31 05:07:07 +0100

    Removed sockets from the home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,59 +1,9 @@
 import { Box, Code, Grid, Text, VStack } from '@chakra-ui/react';
-import WebSocket from 'isomorphic-ws';
 import React from 'react';
-import create from 'zustand';
 import { ColorModeSwitcher, Link, Logo } from '../components';
-import { ROUTE_PLAY, WEB_SOCKET_URL } from '../consts';
-import { Game } from '../Game';
-import { Message } from '../messages';
+import { ROUTE_PLAY } from '../consts';
 
 export const Home: React.FC = () => {
-  const onClick = () => {
-    const socket = new WebSocket(WEB_SOCKET_URL);
-    const game = new Game(socket);
-
-    const useStore = create(() => ({
-      socket: socket,
-      game: game,
-    }));
-
-    socket.onopen = function() {
-      socket.send('{}');
-    };
-
-    socket.onmessage = (incomingMsg) => {
-      const message: Message = JSON.parse(incomingMsg.data.toString());
-      console.log(incomingMsg.data);
-    
-      switch (message.kind) {
-        case 'your-turn':
-          game.changeActivePlayer();
-          // componentDidUpdate() :: React lib
-          game.generateBoard();
-          break;
-      
-        case 'board':
-          game.setBoard(message.data);
-          game.generateBoard();
-          break;
-      
-        case 'player-type':
-          game.setPlayerType(message.colour);
-          break;
-      
-        case 'game-over':
-          // TODO
-          break;
-      
-        case 'game-aborted':
-          // TODO
-          break;
-      
-        default:
-          break;
-      }
-    };
-  };
 
   return (
     <Box textAlign="center" fontSize="xl">
@@ -64,7 +14,7 @@ export const Home: React.FC = () => {
           <Text>
             Edit <Code fontSize="xl"> App.tsx</Code> and save to reload.
           </Text>
-          <Link text="Play Now!" route={ROUTE_PLAY} onClick={onClick} />
+          <Link text="Play Now!" route={ROUTE_PLAY} />
         </VStack>
       </Grid>
     </Box>
